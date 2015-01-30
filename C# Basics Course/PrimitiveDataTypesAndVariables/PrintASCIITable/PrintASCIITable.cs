a5f2995cd3be4e743587bf5e7ade90a23a0bc318
Stanimir Panayotov <stanimirpanayotov2@gmail.com>
2015-01-30 22:25:11 +0200

    Added new assignments

---
+++ C# Basics Course/PrimitiveDataTypesAndVariables/PrintASCIITable/PrintASCIITable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.GetEncoding("UTF-8");
+        byte length = 255;
+
+        for (int i = 0; i < length; i++)
+        {
+            char character = (char) i;
+            Console.WriteLine("{0} -> {1}", i, character);
+        }
+    }
+}
