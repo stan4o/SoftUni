23646495a3229bc60f59533d3ff46463f8eda27f
Stanimir Panayotov <stanimirpanayotov2@gmail.com>
2015-01-31 14:34:31 +0200

    Added Joro the football player solution

---
+++ C# Basics Course/ExamProblems/JoroTheFootballPlayer/JoroTheFootballPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class JoroTheFootballPlayer
+{
+    static void Main()
+    {
+        double plays = 0;
+        int weekends = 52;
+        char leapYear = char.Parse(Console.ReadLine());
+        int holidays = int.Parse(Console.ReadLine());
+        int hometownWeekends = int.Parse(Console.ReadLine());
+        int normalWeekends = weekends - hometownWeekends;
+
+        plays += holidays * 1d / 2d; // holiday plays
+        plays += normalWeekends * 2d / 3d; // normal weekend plays
+        plays += hometownWeekends; // hometown weekend plays
+
+        if (leapYear == 't')
+        {
+            plays += 3;
+        }
+
+        Console.Write(Math.Floor(plays));
+    }
+}
