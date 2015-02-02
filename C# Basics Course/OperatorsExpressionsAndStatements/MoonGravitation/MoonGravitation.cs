ea5d83ec08abbc57cc367b5a4f731068a682de37
Stanimir Panayotov <stanimirpanayotov2@gmail.com>
2015-02-02 19:16:48 +0200

    Initial commit

---
+++ C# Basics Course/OperatorsExpressionsAndStatements/MoonGravitation/MoonGravitation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MoonGravitation
+{
+    static void Main()
+    {
+        double weightOnEarth = 86d;
+        Console.WriteLine("Weight on the Earth -> {0} and weight on the Moon {1}.", weightOnEarth, weightOnEarth * 17d / 100d);
+
+        weightOnEarth = 74.6d;
+        Console.WriteLine("Weight on the Earth -> {0} and weight on the Moon {1}.", weightOnEarth, weightOnEarth * 17d / 100d);
+
+        weightOnEarth = 53.7d;
+        Console.WriteLine("Weight on the Earth -> {0} and weight on the Moon {1}.", weightOnEarth, weightOnEarth * 17d / 100d);
+    }
+}
---
+++ C# Basics Course/OperatorsExpressionsAndStatements/OddOrEvenIntegers/OddOrEvenIntegers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class OddOrEvenIntegers
+{
+    static void Main()
+    {
+        int n = 3;
+        Console.WriteLine("Is {0} odd? {1}", n, n % 2 != 0);
+
+        n = 2;
+        Console.WriteLine("Is {0} odd? {1}", n, n % 2 != 0);
+
+        n = -2;
+        Console.WriteLine("Is {0} odd? {1}", n, n % 2 != 0);
+
+        n = -1;
+        Console.WriteLine("Is {0} odd? {1}", n, n % 2 != 0);
+
+        n = 0;
+        Console.WriteLine("Is {0} odd? {1}", n, n % 2 != 0);
+    }
+}
