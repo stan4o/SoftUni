059fb21db268b027f99ba890d87016f794a38fdb
Stanimir Panayotov <stanimirpanayotov2@gmail.com>
2015-02-04 22:54:45 +0200

    Added new assignments

---
+++ C# Basics Course/OperatorsExpressionsAndStatements/ExtractThirdBit/ExtractThirdBit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ExtractThirdBit
+{
+    static void Main()
+    {
+        int n = 5;
+        Console.WriteLine("Third bit of {0} is {1}", n, (n >> 3) & 1);
+
+        n = 0;
+        Console.WriteLine("Third bit of {0} is {1}", n, (n >> 3) & 1);
+
+        n = 15;
+        Console.WriteLine("Third bit of {0} is {1}", n, (n >> 3) & 1);
+
+        n = 5343;
+        Console.WriteLine("Third bit of {0} is {1}", n, (n >> 3) & 1);
+
+        n = 62241;
+        Console.WriteLine("Third bit of {0} is {1}", n, (n >> 3) & 1);
+    }
+}
---
+++ C# Basics Course/OperatorsExpressionsAndStatements/PrimeNumberCheck/PrimeNumberCheck.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class PrimeNumberCheck
+{
+    static void Main()
+    {
+        int baundary = 100;
+        int n = 1;
+        Console.WriteLine("Is {0} a prime number? {1}", n, isPrime(n));
+
+        n = 2;
+        Console.WriteLine("Is {0} a prime number? {1}", n, isPrime(n));
+
+        n = 3;
+        Console.WriteLine("Is {0} a prime number? {1}", n, isPrime(n));
+
+        n = 4;
+        Console.WriteLine("Is {0} a prime number? {1}", n, isPrime(n));
+
+        n = 9;
+        Console.WriteLine("Is {0} a prime number? {1}", n, isPrime(n));
+
+        n = 97;
+        Console.WriteLine("Is {0} a prime number? {1}", n, isPrime(n));
+
+        n = 51;
+        Console.WriteLine("Is {0} a prime number? {1}", n, isPrime(n));
+
+        n = -3;
+        Console.WriteLine("Is {0} a prime number? {1}", n, isPrime(n));
+
+        n = 0;
+        Console.WriteLine("Is {0} a prime number? {1}", n, isPrime(n));
+        
+    }
+
+    private static bool isPrime(int n)
+    {
+        bool isPrime = false;
+        int i;
+        int k = 0;
+
+        for (i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                k++;
+            }
+        }
+
+        if (k == 2)
+        {
+            isPrime = true;
+        }
+
+        return isPrime;
+    }
+}
