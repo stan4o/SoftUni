4ee9dcf714b74708d1f357294d3412641cbc970e
Stanimir Panayotov <stanimirpanayotov@gmail.com>
2015-02-08 14:53:38 +0200

    Added The Explorer solution

---
+++ C# Basics Course/ExamProblems/TheExplorer/TheExplorer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TheExplorer
+{
+	class TheExplorer
+	{
+		public static void Main ()
+		{
+			int n = int.Parse (Console.ReadLine ());
+
+			Console.Write (new string ('-', n / 2));
+			Console.Write ('*');
+			Console.Write (new string ('-', n / 2) + "\n");
+
+			// upper part
+			int max = n / 2;
+			for (int i = 1; i <= max; i++) {
+				Console.Write (new string ('-', (n / 2) - i));
+				Console.Write ('*');
+				Console.Write (new string ('-', (2 * i) - 1));
+				Console.Write ('*');
+				Console.Write (new string ('-', (n / 2) - i) + "\n");
+			}
+
+			// lower part
+			for (int i = n / 2 - 1; i >= 1; i--) {
+				Console.Write (new string ('-', (n / 2) - i));
+				Console.Write ('*');
+				Console.Write (new string ('-', (2 * i) - 1));
+				Console.Write ('*');
+				Console.Write (new string ('-', (n / 2) - i) + "\n");
+			}
+
+			Console.Write (new string ('-', n / 2));
+			Console.Write ('*');
+			Console.Write (new string ('-', n / 2));
+		}
+	}
+}
