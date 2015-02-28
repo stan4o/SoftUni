6da49e938615f6eb91fc43854d06bafd21486f27
Stanimir Panayotov <stanimirpanayotov@gmail.com>
2015-02-28 20:05:09 +0200

    Added Sum of elements solution

--- C# Basics Course/ExamProblems/README.md
+++ C# Basics Course/ExamProblems/README.md
@@ -1,20 +1,21 @@
 1. C# Basics Exam 10 April 2014 Morning
 ( http://judge.softuni.bg/Contests/Practice/Index/2#0 )
-	- JoroTheFootballPlayer
-	- HalfSum
+	- Joro The Football Player
+	- Half Sum
 	- Sunglasses
-	- NineDigitMagicNumbers
-	- BitsInverter
+	- Nine Digit Magic Numbers
+	- Bits Inverter
 
 2. C# Basics Exam 10 April 2014 Evening
 ( http://judge.softuni.bg/Contests/Practice/Index/3#0 )
 	- Volleyball
 	- Odd / Even Sum
 	- Eclipse
-	- HayvanNumbers
+	- Hayvan Numbers
 	- Bits Up
 
 3. C# Basics Exam 11 April 2014 Morning
 ( http://judge.softuni.bg/Contests/Practice/Index/4#0 )
 	- Work Hours
+	- Sum of elements
 	- The Explorer
---
+++ C# Basics Course/ExamProblems/SumOfElements/SumOfElements.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SumOfElements
+{
+	class SumOfElements
+	{
+		public static void Main()
+		{
+			string input = Console.ReadLine();
+			string[] arrayOfNumbers = input.Split(' ');
+			int arrayLength = arrayOfNumbers.Length;
+			long finalSum = 0;
+			long biggestNumber = long.MinValue;
+
+			for (int i = 0; i < arrayLength; i++)
+			{
+				long currentNumber = long.Parse(arrayOfNumbers[i]);
+				finalSum += currentNumber;
+
+				if (currentNumber > biggestNumber)
+				{
+					biggestNumber = currentNumber;
+				}
+			}
+
+			if (biggestNumber * 2 == finalSum)
+			{
+				Console.WriteLine("Yes, sum=" + biggestNumber);
+			}
+			else
+			{
+				long difference = Math.Abs((biggestNumber * 2) - finalSum);
+				Console.WriteLine("No, diff=" + difference);
+			}
+		}
+	}
+}
